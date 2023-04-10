c160082148758d851347718552150394a9314bfc
Andrés Megías <109989219+andresmegias@users.noreply.github.com>
2023-04-10 13:43:02 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as file:
 
 setuptools.setup(
     name = 'richvalues',
-    version = '2.0.0',
+    version = '3.0.0',
     license = 'BSD-3-Clause',
     author = 'Andrés Megías Toledano',
     description = 'Python library for working with uncertainties and upper/lower limits',
