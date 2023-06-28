1dce1145037151cc7a2cc1021621f689b88b1e54
Andrés Megías <109989219+andresmegias@users.noreply.github.com>
2023-06-29 00:25:08 +0200

    Update __init__.py

--- richvalues/__init__.py
+++ richvalues/__init__.py
@@ -37,7 +37,7 @@ IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 POSSIBILITY OF SUCH DAMAGE.
 """
 
-__version__ = '3.1.4'
+__version__ = '3.1.5'
 __author__ = 'Andrés Megías Toledano'
 
 import copy
